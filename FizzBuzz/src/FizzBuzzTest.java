b18b77202a161651a074aea0960fc19d718f777e
Salma Claure <salmaelianetclaure@gmail.com>
2020-02-10 23:35:05 -0400

    generar Buzz

--- FizzBuzz/src/FizzBuzzTest.java
+++ FizzBuzz/src/FizzBuzzTest.java
@@ -5,8 +5,23 @@ import org.junit.Test;
 public class FizzBuzzTest {
 
 	@Test
-	public void test() {
+	public void generarUno() {
 		FizzBuzz fb= new FizzBuzz();
 		assertEquals("1", fb.generar(1));
 	}
+	
+	@Test
+	public void generarFizz() {
+		FizzBuzz fb= new FizzBuzz();
+		assertEquals("Fizz", fb.generar(3));
+	}
+	
+	@Test
+	public void generarBuzz() {
+		FizzBuzz fb= new FizzBuzz();
+		assertEquals("Buzz", fb.generar(5));
+	}
+	
+
+	
 }
